7fea47aefa30792f6a14a1f16dbb61dcb902c13b
Alex Albu <alexmhh1234@gmail.com>
2023-05-12 10:38:14 +0300

    Initial Commit

---
+++ PadLock/PadLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadLockApp.swift
+//  PadLock
+//
+//  Created by Alex Albu on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PadLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
